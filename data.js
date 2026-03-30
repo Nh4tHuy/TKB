7295a90bede1f9ff7dcbc8980c5bae4cef43a931
Theme Plus <102273066+Nh4tHuy@users.noreply.github.com>
2026-03-30 22:06:49 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,399 @@
+[
+  {
+    "id": "1774880840979",
+    "name": "XL Số liệu",
+    "groups": [
+      {
+        "id": "1774880848233",
+        "name": "1",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774880864768",
+            "day": 3,
+            "start": 6,
+            "end": 9,
+            "room": "..",
+            "teacher": ".."
+          },
+          {
+            "id": "1774880879926",
+            "day": 4,
+            "start": 1,
+            "end": 3,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      },
+      {
+        "id": "1774880882334",
+        "name": "2",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774880890590",
+            "day": 2,
+            "start": 1,
+            "end": 3,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      },
+      {
+        "id": "1774880916133",
+        "name": "3",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774880923221",
+            "day": 3,
+            "start": 1,
+            "end": 3,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1774880940260",
+    "name": "QT Ngẫu nhiên",
+    "groups": [
+      {
+        "id": "1774880950659",
+        "name": "1",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774880962394",
+            "day": 6,
+            "start": 1,
+            "end": 4,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      },
+      {
+        "id": "1774880967537",
+        "name": "2",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774880978583",
+            "day": 4,
+            "start": 4,
+            "end": 5,
+            "room": "..",
+            "teacher": ".."
+          },
+          {
+            "id": "1774880986597",
+            "day": 5,
+            "start": 1,
+            "end": 2,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1774880999937",
+    "name": "LT Đồ thị",
+    "groups": [
+      {
+        "id": "1774881005677",
+        "name": "1",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881016121",
+            "day": 5,
+            "start": 6,
+            "end": 7,
+            "room": "..",
+            "teacher": ".."
+          },
+          {
+            "id": "1774881024493",
+            "day": 6,
+            "start": 6,
+            "end": 7,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      },
+      {
+        "id": "1774881054737",
+        "name": "2",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881063888",
+            "day": 4,
+            "start": 1,
+            "end": 2,
+            "room": "..",
+            "teacher": ".."
+          },
+          {
+            "id": "1774881081515",
+            "day": 6,
+            "start": 4,
+            "end": 5,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1774881102428",
+    "name": "Lịch sử Đảng",
+    "groups": [
+      {
+        "id": "1774881106319",
+        "name": "1",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881129108",
+            "day": 4,
+            "start": 6,
+            "end": 8,
+            "room": "..",
+            "teacher": "C. Chinh"
+          }
+        ]
+      },
+      {
+        "id": "1774881131788",
+        "name": "2",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881160976",
+            "day": 2,
+            "start": 6,
+            "end": 8,
+            "room": "..",
+            "teacher": "C. Chinh"
+          }
+        ]
+      },
+      {
+        "id": "1774881133900",
+        "name": "3",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881170591",
+            "day": 2,
+            "start": 1,
+            "end": 3,
+            "room": "..",
+            "teacher": "C. Chinh"
+          }
+        ]
+      },
+      {
+        "id": "1774881136051",
+        "name": "4",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881191581",
+            "day": 3,
+            "start": 1,
+            "end": 3,
+            "room": "..",
+            "teacher": "C. Lựu"
+          }
+        ]
+      },
+      {
+        "id": "1774881137610",
+        "name": "5",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881207357",
+            "day": 3,
+            "start": 6,
+            "end": 8,
+            "room": "..",
+            "teacher": "C. Lựu"
+          }
+        ]
+      },
+      {
+        "id": "1774881139602",
+        "name": "6",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881221430",
+            "day": 7,
+            "start": 1,
+            "end": 3,
+            "room": "..",
+            "teacher": "C. Xuyến"
+          }
+        ]
+      },
+      {
+        "id": "1774881141659",
+        "name": "7",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881252830",
+            "day": 7,
+            "start": 6,
+            "end": 8,
+            "room": "..",
+            "teacher": "C. Xuyến"
+          }
+        ]
+      },
+      {
+        "id": "1774881143465",
+        "name": "8",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881279645",
+            "day": 5,
+            "start": 6,
+            "end": 8,
+            "room": "..",
+            "teacher": "C. Chinh"
+          }
+        ]
+      },
+      {
+        "id": "1774881145322",
+        "name": "9",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881304074",
+            "day": 3,
+            "start": 6,
+            "end": 8,
+            "room": "..",
+            "teacher": "C. Chinh"
+          }
+        ]
+      },
+      {
+        "id": "1774881148088",
+        "name": "10",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881317005",
+            "day": 6,
+            "start": 6,
+            "end": 8,
+            "room": "..",
+            "teacher": "C. Xuyến"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1774881331605",
+    "name": "Anh văn",
+    "groups": [
+      {
+        "id": "1774881353783",
+        "name": "1",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881359109",
+            "day": 7,
+            "start": 1,
+            "end": 3,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      },
+      {
+        "id": "1774881362309",
+        "name": "2",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881368155",
+            "day": 6,
+            "start": 1,
+            "end": 3,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1774881342491",
+    "name": "GT Phức",
+    "groups": [
+      {
+        "id": "1774881374724",
+        "name": "1",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774881380922",
+            "day": 2,
+            "start": 6,
+            "end": 7,
+            "room": "..",
+            "teacher": ".."
+          },
+          {
+            "id": "1774881388033",
+            "day": 7,
+            "start": 6,
+            "end": 7,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1774883064084",
+    "name": "TK Bayes",
+    "groups": [
+      {
+        "id": "1774883069870",
+        "name": "1",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1774883076322",
+            "day": 7,
+            "start": 6,
+            "end": 9,
+            "room": "..",
+            "teacher": ".."
+          }
+        ]
+      }
+    ]
+  }
+]
