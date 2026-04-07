4e72696d3519a540156cae233dee07fec9c32788
Nhat Huy <102273066+Nh4tHuy@users.noreply.github.com>
2026-04-07 16:17:21 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,396 +1,406 @@
 [
   {
-    "id": "1774880840979",
-    "name": "XL Số liệu",
+    "id": "1775553435228rz4jn",
+    "name": "Xử lý số liệu thống kê",
     "groups": [
       {
-        "id": "1774880848233",
-        "name": "1",
+        "id": "1775553435228p1v2l",
+        "name": "01",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774880864768",
+            "id": "1775553435228qu087",
             "day": 3,
             "start": 6,
             "end": 9,
-            "room": "..",
-            "teacher": ".."
+            "room": "MT1/KH",
+            "teacher": "Thạc sỹ Lê Thị Mỹ Xuân"
           },
           {
-            "id": "1774880879926",
+            "id": "17755534352288ga01",
             "day": 4,
             "start": 1,
             "end": 3,
-            "room": "..",
-            "teacher": ".."
+            "room": "TT03KH",
+            "teacher": "Thạc sỹ Lê Thị Mỹ Xuân"
           }
         ]
       },
       {
-        "id": "1774880882334",
-        "name": "2",
+        "id": "177555343522802q0v",
+        "name": "02",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774880890590",
+            "id": "17755534352289v72p",
             "day": 2,
             "start": 1,
             "end": 3,
-            "room": "..",
-            "teacher": ".."
+            "room": "TT07KH",
+            "teacher": "Tiến sỹ Trần Phước Lộc"
+          },
+          {
+            "id": "1775553435228ts5ya",
+            "day": 4,
+            "start": 6,
+            "end": 8,
+            "room": "MT1/KH",
+            "teacher": "Tiến sỹ Trần Phước Lộc"
           }
         ]
       },
       {
-        "id": "1774880916133",
-        "name": "3",
+        "id": "177555343522888cbx",
+        "name": "03",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774880923221",
+            "id": "1775553435228a584h",
             "day": 3,
             "start": 1,
             "end": 3,
-            "room": "..",
-            "teacher": ".."
+            "room": "TT03KH",
+            "teacher": "Tiến sỹ Trần Phước Lộc"
+          },
+          {
+            "id": "1775553435228254e4",
+            "day": 4,
+            "start": 1,
+            "end": 3,
+            "room": "MT1/KH",
+            "teacher": "Tiến sỹ Trần Phước Lộc"
           }
         ]
       }
     ]
   },
   {
-    "id": "1774880940260",
-    "name": "QT Ngẫu nhiên",
+    "id": "17755534352283xmta",
+    "name": "Quá trình ngẫu nhiên",
     "groups": [
       {
-        "id": "1774880950659",
-        "name": "1",
+        "id": "1775553435228p1yij",
+        "name": "01",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774880962394",
+            "id": "1775553435228duuo4",
             "day": 6,
             "start": 1,
             "end": 4,
-            "room": "..",
-            "teacher": ".."
+            "room": "103/A3",
+            "teacher": "Tiến sỹ Lâm Hoàng Chương"
           }
         ]
       },
       {
-        "id": "1774880967537",
-        "name": "2",
+        "id": "1775553435228yhvtc",
+        "name": "02",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774880978583",
+            "id": "1775553435228amdb9",
             "day": 4,
             "start": 4,
             "end": 5,
-            "room": "..",
-            "teacher": ".."
+            "room": "212/B1",
+            "teacher": "Tiến sỹ Lâm Hoàng Chương"
           },
           {
-            "id": "1774880986597",
+            "id": "17755534352283p80a",
             "day": 5,
             "start": 1,
             "end": 2,
-            "room": "..",
-            "teacher": ".."
+            "room": "101/A3",
+            "teacher": "Tiến sỹ Lâm Hoàng Chương"
           }
         ]
       }
     ]
   },
   {
-    "id": "1774880999937",
-    "name": "LT Đồ thị",
+    "id": "1775553435228bbz6n",
+    "name": "Lý thuyết đồ thị và ứng dụng",
     "groups": [
       {
-        "id": "1774881005677",
-        "name": "1",
+        "id": "1775553435228uclm3",
+        "name": "01",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881016121",
+            "id": "177555343522839yh1",
             "day": 5,
             "start": 6,
             "end": 7,
-            "room": "..",
-            "teacher": ".."
+            "room": "204/KH",
+            "teacher": "Tiến sỹ Phạm Bích Như"
           },
           {
-            "id": "1774881024493",
+            "id": "1775553435228mpqjz",
             "day": 6,
             "start": 6,
             "end": 7,
-            "room": "..",
-            "teacher": ".."
+            "room": "205/KH",
+            "teacher": "Tiến sỹ Phạm Bích Như"
           }
         ]
       },
       {
-        "id": "1774881054737",
-        "name": "2",
+        "id": "1775553435228n07q1",
+        "name": "02",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881063888",
+            "id": "1775553435228hhb1k",
             "day": 4,
             "start": 1,
             "end": 2,
-            "room": "..",
-            "teacher": ".."
+            "room": "304/D2",
+            "teacher": "Tiến sỹ Phạm Bích Như"
           },
           {
-            "id": "1774881081515",
+            "id": "1775553435228ogzd7",
             "day": 6,
             "start": 4,
             "end": 5,
-            "room": "..",
-            "teacher": ".."
+            "room": "207/KH",
+            "teacher": "Tiến sỹ Phạm Bích Như"
           }
         ]
       }
     ]
   },
   {
-    "id": "1774881102428",
-    "name": "Lịch sử Đảng",
+    "id": "1775553435228k3hnn",
+    "name": "Giải tích phức",
     "groups": [
       {
-        "id": "1774881106319",
-        "name": "1",
+        "id": "1775553435228ttx1s",
+        "name": "01",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881129108",
+            "id": "17755534352285u2ly",
+            "day": 2,
+            "start": 6,
+            "end": 7,
+            "room": "209/KH",
+            "teacher": "Tiến sỹ Đinh Ngọc Quý"
+          },
+          {
+            "id": "1775553435228g9aa7",
+            "day": 7,
+            "start": 6,
+            "end": 7,
+            "room": "204/KH",
+            "teacher": "Tiến sỹ Đinh Ngọc Quý"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "17755534352287ezs2",
+    "name": "Anh văn chuyên môn - TTK",
+    "groups": [
+      {
+        "id": "1775553435228ofkqp",
+        "name": "01",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1775553435228l78gp",
+            "day": 7,
+            "start": 1,
+            "end": 3,
+            "room": "205/KH",
+            "teacher": "Tiến sỹ Nguyễn Thành Quí"
+          }
+        ]
+      },
+      {
+        "id": "1775553435228ibiqk",
+        "name": "02",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1775553435228zx8mc",
+            "day": 6,
+            "start": 1,
+            "end": 3,
+            "room": "209/KH",
+            "teacher": "Tiến sỹ Nguyễn Thành Quí"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1775553435228u12pd",
+    "name": "Lịch sử Đảng Cộng sản Việt Nam",
+    "groups": [
+      {
+        "id": "17755534352286nfnr",
+        "name": "01",
+        "pinned": false,
+        "sessions": [
+          {
+            "id": "1775553435228wgtpv",
             "day": 4,
             "start": 6,
             "end": 8,
-            "room": "..",
-            "teacher": "C. Chinh"
+            "room": "106/C1",
+            "teacher": "Tiến sỹ Đinh Thị Chinh"
           }
         ]
       },
       {
-        "id": "1774881131788",
-        "name": "2",
+        "id": "17755534352288hfss",
+        "name": "02",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881160976",
+            "id": "177555343522881vb8",
             "day": 2,
             "start": 6,
             "end": 8,
-            "room": "..",
-            "teacher": "C. Chinh"
+            "room": "107/C1",
+            "teacher": "Tiến sỹ Đinh Thị Chinh"
           }
         ]
       },
       {
-        "id": "1774881133900",
-        "name": "3",
+        "id": "1775553435228nmhwm",
+        "name": "03",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881170591",
+            "id": "1775553435228hp58x",
             "day": 2,
             "start": 1,
             "end": 3,
-            "room": "..",
-            "teacher": "C. Chinh"
+            "room": "107/C1",
+            "teacher": "Tiến sỹ Đinh Thị Chinh"
           }
         ]
       },
       {
-        "id": "1774881136051",
-        "name": "4",
+        "id": "1775553435228n7jgr",
+        "name": "04",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881191581",
+            "id": "1775553435228vjse5",
             "day": 3,
             "start": 1,
             "end": 3,
-            "room": "..",
-            "teacher": "C. Lựu"
+            "room": "HTR/DB",
+            "teacher": "Thạc sỹ Nguyễn Thị Thúy Lựu"
           }
         ]
       },
       {
-        "id": "1774881137610",
-        "name": "5",
+        "id": "1775553435228sjf3a",
+        "name": "05",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881207357",
+            "id": "17755534352285njkk",
             "day": 3,
             "start": 6,
             "end": 8,
-            "room": "..",
-            "teacher": "C. Lựu"
+            "room": "102/KT",
+            "teacher": "Thạc sỹ Nguyễn Thị Thúy Lựu"
           }
         ]
       },
       {
-        "id": "1774881139602",
-        "name": "6",
+        "id": "1775553435228z7w65",
+        "name": "06",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881221430",
+            "id": "1775553435228wa7an",
             "day": 7,
             "start": 1,
             "end": 3,
-            "room": "..",
-            "teacher": "C. Xuyến"
+            "room": "102/KT",
+            "teacher": "Tiến sỹ Đồng Thị Kim Xuyến"
           }
         ]
       },
       {
-        "id": "1774881141659",
-        "name": "7",
+        "id": "1775553435228uhfq5",
+        "name": "07",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881252830",
+            "id": "17755534352284ok1b",
             "day": 7,
             "start": 6,
             "end": 8,
-            "room": "..",
-            "teacher": "C. Xuyến"
+            "room": "102/KT",
+            "teacher": "Tiến sỹ Đồng Thị Kim Xuyến"
           }
         ]
       },
       {
-        "id": "1774881143465",
-        "name": "8",
+        "id": "17755534352285f7q4",
+        "name": "08",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881279645",
+            "id": "1775553435228lent0",
             "day": 5,
             "start": 6,
             "end": 8,
-            "room": "..",
-            "teacher": "C. Chinh"
+            "room": "103/KH",
+            "teacher": "Tiến sỹ Đinh Thị Chinh"
           }
         ]
       },
       {
-        "id": "1774881145322",
-        "name": "9",
+        "id": "1775553435228dgvnx",
+        "name": "09",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881304074",
+            "id": "1775553435228gh66x",
             "day": 3,
             "start": 6,
             "end": 8,
-            "room": "..",
-            "teacher": "C. Chinh"
+            "room": "203/KH",
+            "teacher": "Tiến sỹ Đinh Thị Chinh"
           }
         ]
       },
       {
-        "id": "1774881148088",
+        "id": "1775553435228qbdsb",
         "name": "10",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881317005",
+            "id": "1775553435228a55tg",
             "day": 6,
             "start": 6,
             "end": 8,
-            "room": "..",
-            "teacher": "C. Xuyến"
-          }
-        ]
-      }
-    ]
-  },
-  {
-    "id": "1774881331605",
-    "name": "Anh văn",
-    "groups": [
-      {
-        "id": "1774881353783",
-        "name": "1",
-        "pinned": false,
-        "sessions": [
-          {
-            "id": "1774881359109",
-            "day": 7,
-            "start": 1,
-            "end": 3,
-            "room": "..",
-            "teacher": ".."
+            "room": "203/KH",
+            "teacher": "Tiến sỹ Đồng Thị Kim Xuyến"
           }
         ]
       },
       {
-        "id": "1774881362309",
-        "name": "2",
+        "id": "17755534352280j0yf",
+        "name": "H01",
         "pinned": false,
         "sessions": [
           {
-            "id": "1774881368155",
-            "day": 6,
-            "start": 1,
-            "end": 3,
-            "room": "..",
-            "teacher": ".."
-          }
-        ]
-      }
-    ]
-  },
-  {
-    "id": "1774881342491",
-    "name": "GT Phức",
-    "groups": [
-      {
-        "id": "1774881374724",
-        "name": "1",
-        "pinned": false,
-        "sessions": [
-          {
-            "id": "1774881380922",
-            "day": 2,
+            "id": "1775553435228i0497",
+            "day": 5,
             "start": 6,
-            "end": 7,
-            "room": "..",
-            "teacher": ".."
-          },
-          {
-            "id": "1774881388033",
-            "day": 7,
-            "start": 6,
-            "end": 7,
-            "room": "..",
-            "teacher": ".."
-          }
-        ]
-      }
-    ]
-  },
-  {
-    "id": "1774883064084",
-    "name": "TK Bayes",
-    "groups": [
-      {
-        "id": "1774883069870",
-        "name": "1",
-        "pinned": false,
-        "sessions": [
-          {
-            "id": "1774883076322",
-            "day": 7,
-            "start": 6,
-            "end": 9,
-            "room": "..",
-            "teacher": ".."
+            "end": 8,
+            "room": "101HA1",
+            "teacher": "Thạc sỹ Nguyễn Thị Thúy Lựu"
           }
         ]
       }
